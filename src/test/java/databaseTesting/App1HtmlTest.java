42051c4a3bb4498d5aef5d775553e4944a98f38a
Java2014 <jan.ala.vahala@gmail.com>
2025-04-04 13:21:04 +0200

    Small change of App1.hml

--- src/test/java/databaseTesting/App1HtmlTest.java
+++ src/test/java/databaseTesting/App1HtmlTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class App1HtmlTesting {
+public class App1HtmlTest {
 
 	private WebDriver driver;
 
